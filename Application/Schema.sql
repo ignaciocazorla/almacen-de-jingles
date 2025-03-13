3f629d84e98d993b2ca6af795daf29c52018fc77
Ignacio <nacla12@hotmail.com>
2025-03-12 21:24:12 -0300

    Agrega tabla jingles

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,11 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE jingles (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    nombre TEXT NOT NULL,
+    fecha DATE DEFAULT NULL,
+    link TEXT NOT NULL,
+    tiempo_inicio TEXT DEFAULT NULL,
+    nombre_video TEXT NOT NULL,
+    banda_original TEXT DEFAULT NULL,
+    creado_por TEXT DEFAULT NULL
+);
